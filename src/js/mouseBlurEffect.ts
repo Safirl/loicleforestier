9afef2a8ae9ccfd3ece41f0ad73be601500e9413
Safirl <loic.leforestier@mmibordeaux.com>
2025-03-22 16:06:40 +0100

    fixing background bug

--- src/js/mouseBlurEffect.ts
+++ src/js/mouseBlurEffect.ts
@@ -4,26 +4,25 @@ const mouseFollower = document.getElementById("mouseFollower");
 const trailContainer = document.getElementById("trail");
 
 function followMouse(image: HTMLImageElement) {
-    if (mouseFollower && trailContainer && image) {;
+    if (mouseFollower && trailContainer && image) {
         //Get dominent color
-        image.onload = () => {
+        image.addEventListener('load', () => {
             const colorThief = new ColorThief();
-            const dominantColor = colorThief.getColor(image); 
+            const dominantColor = colorThief.getColor(image);
             const darkenFactor = .8
-            const rgbColor = `rgb(${dominantColor[0]*darkenFactor}, ${dominantColor[1]*darkenFactor}, ${dominantColor[2]*darkenFactor})`
+            const rgbColor = `rgb(${dominantColor[0] * darkenFactor}, ${dominantColor[1] * darkenFactor}, ${dominantColor[2] * darkenFactor})`
             mouseFollower.style.backgroundColor = rgbColor
-        };
-    
+        });
+
         //move the mouseFollower
         document.addEventListener("mousemove", (e) => {
             const mouseX = e.clientX;
             const mouseY = e.clientY;
-            console.log(mouseX, " ", mouseY)
             if (!mouseFollower || !trailContainer) return;
-            mouseFollower.style.top = mouseY.toString() + "px" 
+            mouseFollower.style.top = mouseY.toString() + "px"
             mouseFollower.style.left = mouseX.toString() + "px"
         })
     }
 }
 
-export {followMouse};
+export { followMouse };
