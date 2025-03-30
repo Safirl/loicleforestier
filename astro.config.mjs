99b7761d5fb2414f97c743f902648370d4a0d02e
Safirl <loic.leforestier@mmibordeaux.com>
2025-03-30 14:52:31 +0200

    trying to optimise perfs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  devToolbar: {
-    enabled: false,
-  },
+  // devToolbar: {
+  //   enabled: false,
+  // },
 });
