8b7171ddd561362d810ec79bcb88856294e46e3b
Loïc Leforestier <loicleforestier.pro@gmail.com>
2025-11-02 18:19:21 +0100

    added project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,124 +1,148 @@
 const projects = [
   {
     id: 0,
-    title: 'About me',
-    description: 'I\'m Loïc Leforestier, a french technical designer and artist based in Paris. I create projects to connect people with each others.',
+    title: "About me",
+    description:
+      "I'm Loïc Leforestier, a french technical designer and artist based in Paris. I create projects to connect people with each others.",
     image: "/images/gallery.webp",
-    category: '',
-    link: '/profile',
-    secondaryLink: '',
-    secondaryLinkName: ""
+    category: "",
+    link: "/profile",
+    secondaryLink: "",
+    secondaryLinkName: "",
   },
   {
     id: 1,
-    title: 'Cat world',
-    description: 'When we grow up, we often lose contact with close friends or family members, because of our studies, work or the distance between us. Cat world wants to bring people together, by sending them personal letters where you can reveal your feelings. And you can also create your own cat.',
+    title: "Cat world",
+    description:
+      "When we grow up, we often lose contact with close friends or family members, because of our studies, work or the distance between us. Cat world wants to bring people together, by sending them personal letters where you can reveal your feelings. And you can also create your own cat.",
     image: "/images/catworld.webp",
-    category: 'dev',
-    link: 'https://frontend-cat-world.osc-fr1.scalingo.io/',
-    secondaryLink: 'https://github.com/Safirl/cat-world',
-    secondaryLinkName: "Github"
+    category: "dev",
+    link: "https://frontend-cat-world.osc-fr1.scalingo.io/",
+    secondaryLink: "https://github.com/Safirl/cat-world",
+    secondaryLinkName: "Github",
   },
   {
     id: 2,
-    title: 'Nuit du musée',
-    description: 'Interactive experience made for the MUSBA of Bordeaux, displayed on a 4K screen at the museum. A collaborative work to make visitors discover the reserve of the MUSBA and the unique paintings it has to show.',
+    title: "Nuit du musée",
+    description:
+      "Interactive experience made for the MUSBA of Bordeaux, displayed on a 4K screen at the museum. A collaborative work to make visitors discover the reserve of the MUSBA and the unique paintings it has to show.",
     image: "/images/reserve.webp",
-    category: 'dev',
-    link: 'https://nuit-du-musba-2025.netlify.app/experiences/1-hub/index.html',
-    secondaryLink: 'https://github.com/nuit-musee-musba/experience-2025',
-    secondaryLinkName: "Github"
+    category: "dev",
+    link: "https://tv-watcher-gobz.netlify.app/",
+    secondaryLink: "https://github.com/Safirl/back-to-basics-rendu",
+    secondaryLinkName: "Github",
+  },
+  {
+    id: 12,
+    title: "TV Watcher",
+    description:
+      "A small experience made for a Gobelins' course that questions population control through medias.",
+    image: "/images/tv-watcher.webp",
+    category: "dev",
+    link: "https://safirl.itch.io/cib-au-boulot",
+    secondaryLink: "https://github.com/MatthieuMarchand/Ciboulot",
+    secondaryLinkName: "Github",
   },
   {
     id: 11,
-    title: 'Cib\' au boulot!',
-    description: 'A parodic game based on the Animal crossing universe by Nitendo. The player plays Cib\', a comedian that is having a hard time finding a job. One day, he wakes up in the Nitenders boss\' office. He is asked to make him laugh to have a role in their last game. But the boss has a... special sense of humor.',
+    title: "Cib' au boulot!",
+    description:
+      "A parodic game based on the Animal crossing universe by Nitendo. The player plays Cib', a comedian that is having a hard time finding a job. One day, he wakes up in the Nitenders boss' office. He is asked to make him laugh to have a role in their last game. But the boss has a... special sense of humor.",
     image: "/images/ciboulot.webp",
-    category: 'dev',
-    link: 'https://safirl.itch.io/cib-au-boulot',
-    secondaryLink: 'https://github.com/MatthieuMarchand/Ciboulot',
-    secondaryLinkName: "Github"
+    category: "dev",
+    link: "https://safirl.itch.io/cib-au-boulot",
+    secondaryLink: "https://github.com/MatthieuMarchand/Ciboulot",
+    secondaryLinkName: "Github",
   },
   {
     id: 3,
-    title: 'Smash',
-    category: 'dev',
-    description: 'Misinformation and fake news are the two main threats of journalism and freedom of speech nowadays. Smash wants to confront its users with fake news. Everyday, they can respond to a survey and guess if some selected news are fake or not, and then have a some key elements to understand why. ',
+    title: "Smash",
+    category: "dev",
+    description:
+      "Misinformation and fake news are the two main threats of journalism and freedom of speech nowadays. Smash wants to confront its users with fake news. Everyday, they can respond to a survey and guess if some selected news are fake or not, and then have a some key elements to understand why. ",
     image: "/images/smash.webp",
-    link: 'https://flip-app.osc-fr1.scalingo.io/',
-    secondaryLink: 'https://github.com/Safirl/Flip',
-    secondaryLinkName: "Github"
+    link: "https://flip-app.osc-fr1.scalingo.io/",
+    secondaryLink: "https://github.com/Safirl/Flip",
+    secondaryLinkName: "Github",
   },
   {
     id: 4,
-    title: 'Nous n\'avons pas numérisé',
-    description: 'If your only tool is a hammer, then every problem looks like a nail. An essay of how we decided to not digitize an application and a post mortem analysis of our mistakes on this subject.',
+    title: "Nous n'avons pas numérisé",
+    description:
+      "If your only tool is a hammer, then every problem looks like a nail. An essay of how we decided to not digitize an application and a post mortem analysis of our mistakes on this subject.",
     image: "/images/nousnavonspasnumerise.webp",
-    category: 'dev',
-    link: 'https://nousnavonspasnumerise.mmibordeaux.com/',
-    secondaryLink: 'https://github.com/osunyorg/bordeauxmontaigne-nousnavonspasnumerise',
-    secondaryLinkName: "Github"
+    category: "dev",
+    link: "https://nousnavonspasnumerise.mmibordeaux.com/",
+    secondaryLink:
+      "https://github.com/osunyorg/bordeauxmontaigne-nousnavonspasnumerise",
+    secondaryLinkName: "Github",
   },
   {
     id: 5,
-    title: 'Gallery',
-    description: 'I\'ve been sharing my artwork on Instagram since my first year of studies, back in 2020. I like to explore different art styles through photomontages, short films, photographs or drawings.',
+    title: "Gallery",
+    description:
+      "I've been sharing my artwork on Instagram since my first year of studies, back in 2020. I like to explore different art styles through photomontages, short films, photographs or drawings.",
     image: "/images/gallery.webp",
-    category: 'movie',
-    link: 'https://www.instagram.com/loicl_photographie/',
-    secondaryLink: '',
-    secondaryLinkName: ""
+    category: "movie",
+    link: "https://www.instagram.com/loicl_photographie/",
+    secondaryLink: "",
+    secondaryLinkName: "",
   },
   {
     id: 6,
-    title: 'Maria',
-    category: 'dev',
-    description: 'Maria is an interactive game made for the MUSBA of Bordeaux. Players are invited by and old and mysterious woman to play with different paintings and change their destiny...',
+    title: "Maria",
+    category: "dev",
+    description:
+      "Maria is an interactive game made for the MUSBA of Bordeaux. Players are invited by and old and mysterious woman to play with different paintings and change their destiny...",
     image: "/images/maria.webp",
-    link: 'https://maria-bacchanight.netlify.app/',
-    secondaryLink: 'https://github.com/MatthieuMarchand/Maria',
-    secondaryLinkName: "Github"
+    link: "https://maria-bacchanight.netlify.app/",
+    secondaryLink: "https://github.com/MatthieuMarchand/Maria",
+    secondaryLinkName: "Github",
   },
   {
     id: 7,
-    title: 'Dans ta serre',
-    category: 'movie',
-    description: 'A french pilot episode made for a fictive show entitled "Dans ta serre", an infotainment series focused on plants and flowers. The columns are often satirical and copies the style of french shows like Quotidien.',
+    title: "Dans ta serre",
+    category: "movie",
+    description:
+      'A french pilot episode made for a fictive show entitled "Dans ta serre", an infotainment series focused on plants and flowers. The columns are often satirical and copies the style of french shows like Quotidien.',
     image: "/images/danstaserre.webp",
-    link: 'https://www.youtube.com/watch?v=OEWmOXCXh24',
-    secondaryLink: '',
-    secondaryLinkName: ""
+    link: "https://www.youtube.com/watch?v=OEWmOXCXh24",
+    secondaryLink: "",
+    secondaryLinkName: "",
   },
   {
     id: 8,
-    title: 'Music player',
-    category: 'dev',
-    description: 'Music player is a simple music player, made for IOS devices. It focuses on basic features, like adding new music, favorites musics or controlling it through the IOS control center.',
+    title: "Music player",
+    category: "dev",
+    description:
+      "Music player is a simple music player, made for IOS devices. It focuses on basic features, like adding new music, favorites musics or controlling it through the IOS control center.",
     image: "/images/musicplayer.webp",
-    link: 'https://github.com/Safirl/Music-Player',
-    secondaryLink: '',
-    secondaryLinkName: ""
+    link: "https://github.com/Safirl/Music-Player",
+    secondaryLink: "",
+    secondaryLinkName: "",
   },
   {
     id: 9,
-    title: 'Ciné Haïku : Voisin',
-    description: 'A ghost story fan movie. Made for the "Ciné-Haïku" contest. Participants have to make a short movie with only 3 static shots, to celebrate the japanese haïkus made of 3 verses. In this story, we follow a ghost that bear in the streets.',
-    category: 'movie',
+    title: "Ciné Haïku : Voisin",
+    description:
+      'A ghost story fan movie. Made for the "Ciné-Haïku" contest. Participants have to make a short movie with only 3 static shots, to celebrate the japanese haïkus made of 3 verses. In this story, we follow a ghost that bear in the streets.',
+    category: "movie",
     image: "/images/voisin.webp",
-    link: 'https://www.youtube.com/watch?v=wfGicOPzt0k',
-    secondaryLink: '',
-    secondaryLinkName: ""
+    link: "https://www.youtube.com/watch?v=wfGicOPzt0k",
+    secondaryLink: "",
+    secondaryLinkName: "",
   },
   {
     id: 10,
-    title: 'Ciné Haïku : Promesse',
-    description: 'Made for the "Ciné-Haïku" contest, this short movie asks the viewers of what is a "promesse" (promise) and how to represent it visually.',
-    category: 'movie',
+    title: "Ciné Haïku : Promesse",
+    description:
+      'Made for the "Ciné-Haïku" contest, this short movie asks the viewers of what is a "promesse" (promise) and how to represent it visually.',
+    category: "movie",
     image: "/images/promesse.webp",
-    link: 'https://www.youtube.com/watch?v=QCWs_PdgSe4',
-    secondaryLink: '',
-    secondaryLinkName: ""
+    link: "https://www.youtube.com/watch?v=QCWs_PdgSe4",
+    secondaryLink: "",
+    secondaryLinkName: "",
   },
-]
+];
 
 export default projects;
